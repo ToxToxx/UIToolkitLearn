8310ef14cfd9818a170fde74f0808bf3b9557151
Matvey <sinema3d00@gmail.com>
2024-07-29 15:03:38 +0300

    made animation of open and slosing bottom sheet

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,13 +5,19 @@ using UnityEngine.UIElements;
 
 public class UIController : MonoBehaviour
 {
-    //grab our bottom sheet elements
+    //made open and close bottom sheet
     private VisualElement _bottomContainer;
     private Button _openButton;
     private Button _closeButton;
 
+    //made animation of bottom sheet
+    private VisualElement _bottomSheet;
+    private VisualElement _scrim;
+
     private void Start()
     {
+        #region Bottom Container, Open and Close Button Logic
+
         var root = GetComponent<UIDocument>().rootVisualElement;
 
         _bottomContainer = root.Q<VisualElement>("Container_Bottom");
@@ -23,15 +29,33 @@ public class UIController : MonoBehaviour
 
         _openButton.RegisterCallback<ClickEvent>(OnOpenButtonClicked);
         _closeButton.RegisterCallback<ClickEvent>(OnCloseButtonClicked);
+
+        #endregion
+
+        #region BottomSheet and Scrim Variables
+
+        _bottomSheet = root.Q<VisualElement>("BottomSheet");
+        _scrim = root.Q<VisualElement>("Scrim");
+
+        #endregion
     }
 
     private void OnOpenButtonClicked(ClickEvent evt)
     {
+        //display bottom container
         _bottomContainer.style.display = DisplayStyle.Flex;
+
+        //play animation
+        _bottomSheet.AddToClassList("bottomsheet--up");
+        _scrim.AddToClassList("scrim--fadein");
     }
 
     private void OnCloseButtonClicked(ClickEvent evt)
     {
         _bottomContainer.style.display = DisplayStyle.None;
+
+        //making loop animation
+        _bottomSheet.RemoveFromClassList("bottomsheet--up");
+        _scrim.RemoveFromClassList("scrim--fadein");
     }
 }
