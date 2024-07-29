f89a3eccc950dde28199e790e9b65056f7c28e76
Matvey <sinema3d00@gmail.com>
2024-07-29 14:36:50 +0300

    making UI controller script

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIController : MonoBehaviour
+{
+    private VisualElement _bottomContainer;
+    private Button _openButton;
+    private Button _closeButton;
+
+    private void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        _bottomContainer = root.Q<VisualElement>("Container_Bottom");
+
+        _openButton = root.Q<Button>("Button_Open");
+        _closeButton = root.Q<Button>("Button_Close");
+
+        _bottomContainer.style.display = DisplayStyle.None;
+    }
+}
